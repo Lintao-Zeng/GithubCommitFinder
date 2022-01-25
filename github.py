66c77107b0995be35b234f6f678471d8edde0cfe
Lintao-Zeng <2534324260@qq.com>
2022-01-25 15:18:02 +0800

    GithubCommitFinder

---
+++ github.py
@@ -0,0 +1,14 @@
+# _*_ coding:utf-8 _*_
+import urllib2
+
+ua_headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36'}
+page = 34
+
+# 每页有35条数据，所以循环次数是commit总次数除以35取整，这里测试1000次
+for i in range(1000):
+    print('第' + str(i + 1) + '次开始：')
+    with open("output.txt","a") as f:
+        f.write(urllib2.urlopen(urllib2.Request('https://github.com/termux/termux-packages/commits/master?after=6b95718c6054c611ed07bbb8139e0d6ce6e7d593+' + str(page) + '&branch=master',headers=ua_headers)).read())
+    page = page + 35
+    print('第' + str(i + 1) + '次结束：')
+print("Finished!")
