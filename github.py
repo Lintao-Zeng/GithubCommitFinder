03f9cf7fa2db18118d590b3343442b955a05a71a
zenglintao <2534324260@qq.com>
2023-03-21 22:22:28 +0800

    Update github.py

--- github.py
+++ github.py
@@ -1,7 +1,5 @@
 # _*_ coding:utf-8 _*_
 
-# import urllib2 (python2用)
-
 import urllib.request # python3用
 
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36'}
@@ -11,7 +9,7 @@ page = 34
 for i in range(1000):
     print('第' + str(i + 1) + '次开始：')
     with open("output.txt","a") as f:
-        f.write(urllib.request.urlopen(urllib.request.urlopen('https://github.com/termux/termux-packages/commits/master?after=6b95718c6054c611ed07bbb8139e0d6ce6e7d593+' + str(page) + '&branch=master', headers)).read())
+        f.write(urllib.request.urlopen(urllib.request.urlopen("https://github.com/termux/termux-packages/commits/master?after=6b95718c6054c611ed07bbb8139e0d6ce6e7d593+" + str(page) + "&branch=master", headers)).read())
     page = page + 35
     print('第' + str(i + 1) + '次结束：')
 print("Finished!")
