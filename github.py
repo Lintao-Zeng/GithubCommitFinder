23e487c4b99931f2b20bf52b7d2b1149ffb5088e
zenglintao <2534324260@qq.com>
2023-03-21 23:26:11 +0800

    Update github.py

--- github.py
+++ github.py
@@ -1,6 +1,7 @@
-import urllib.request # python3用
+# python3
+import urllib.request
 
-headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36'}
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'}
 page = 34
 
 # 每页有35条数据，所以循环次数是commit总次数除以35取整，这里测试1000次
@@ -8,7 +9,9 @@ for i in range(1000):
     print('第' + str(i + 1) + '次开始：')
     with open("output.txt","a") as f:
         url = 'https://github.com/termux/termux-packages/commits/master?after=6b95718c6054c611ed07bbb8139e0d6ce6e7d593+' + str(page) + '&branch=master'
-        print(urllib.request.urlopen(url.encode('utf-8'), headers).read())
+        request = urllib.request.Request(url=url, headers=headers)
+        html= urllib.request.urlopen(request)
+        f.write(html.read())
     page = page + 35
     print('第' + str(i + 1) + '次结束：')
 print("Finished!")
