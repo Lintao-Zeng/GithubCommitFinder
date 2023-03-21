27e520840c2fb906c718dbc9671616075c40b0e1
zenglintao <2534324260@qq.com>
2023-03-21 23:40:21 +0800

    Update github.py

--- github.py
+++ github.py
@@ -4,11 +4,11 @@ import urllib.request
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'}
 page = 34
 
-# 每页有35条数据，所以循环次数是commit总次数除以35取整，这里测试10次
-for i in range(10):
+# 每页有35条数据，所以循环次数是commit总次数除以35取整,commit 5839次，除以35取整是167
+for i in range(167):
     print('第' + str(i + 1) + '次开始：')
     with open("output.txt","wb") as f:
-        url = 'https://github.com/termux/termux-packages/commits/master?after=6b95718c6054c611ed07bbb8139e0d6ce6e7d593+' + str(page) + '&branch=master'
+        url = 'https://github.com/coolsnowwolf/lede/commits/master?after=229ba8e4a3ace1ea298fdf493746d8994cf0c73a+' + str(page) + '&branch=master&qualified_name=refs%2Fheads%2Fmaster'
         request = urllib.request.Request(url=url, headers=headers)
         html= urllib.request.urlopen(request)
         f.write(html.read())
